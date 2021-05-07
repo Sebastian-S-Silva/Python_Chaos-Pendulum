e3cd1998c9ced5570a5e35e8b77da4504be6ea99
Sebastian.Silva <s.silva@campus.lmu.de>
2021-05-07 18:28:06 +0200

    update

--- pendulum.py
+++ pendulum.py
@@ -2,4 +2,7 @@ import numpy as numpy
 
 class Pendulum():
 
-	def __init__(self, length: float, mass: float = 1, )
+	def __init__(self, length: float, mass: float = 1, angle: float)
+
+		self.start_angle = start_angle
+		self.position = np.arcson
