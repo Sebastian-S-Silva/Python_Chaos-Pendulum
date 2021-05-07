16ce46162e6c37783b1ecda825122cf56b67b34f
Bastian Spatz <bastian.spatz@mnet-mail.de>
2021-05-07 18:22:26 +0200

    Basic sceleton

---
+++ src/pendulum.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pygame as p
+
+class Pendulum():
+
+
+	def __init__(self, length: float, mass: float = 1, start_angle: float):
+		"""
+		initialize a single point pendulum.
+		"""
+		self.start_angle = start_angle
+		self.position = 
+		self.length = length
+		self.mass = mass
+
+	def update_position(self):
+
+	def update_acceleration(self):
+
+	def update_velocity(self):
+
+	def simulate(self):
+
+
+
+
