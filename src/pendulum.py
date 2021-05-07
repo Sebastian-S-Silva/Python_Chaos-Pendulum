fce11e934b18b4bf04b86716e2500a9869261bf9
Sebastian.Silva <s.silva@campus.lmu.de>
2021-05-07 19:17:25 +0200

    Try

--- src/pendulum.py
+++ src/pendulum.py
@@ -1,10 +1,11 @@
 import numpy as np
-import pygame as p
+import pygame as pg
+from scipy.integrate import odeint
 
 class Pendulum():
 
 
-	def __init__(self, length: float, mass: float = 1, start_angle: float):
+	def __init__(self, length: float, mass: float = 1, start_angle: float, friction: float = 0):
 		"""
 		initialize a single point pendulum.
 		"""
@@ -12,15 +13,22 @@ class Pendulum():
 		self.position = 
 		self.length = length
 		self.mass = mass
+		self.friction = friction
 
-	def update_position(self):
+	def physics(y, t, b, c):
+		theta, omega = y
+		dydt = [omega, -b*omega - c*np.sin(theta)]
+		
+		return dydt
 
-	def update_acceleration(self):
 
-	def update_velocity(self):
+	def simulate(self, time):
+		y_start = [np.pi/2, 0]
+		b = 0
+		c = 9.81
+		solutions = odeint(self.physics, y_start, time, args = (b, c))
 
-	def simulate(self):
-
+		return solutions
 
 
 
