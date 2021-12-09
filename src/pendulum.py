ec647814829b97029c42c354141102568e12bc89
Sebastian S Silva <Silva.Sebastian@protonmail.com>
2021-12-09 15:09:02 +0100

    pygame screen, pendulum stand, spinning demo pend

--- src/pendulum.py
+++ src/pendulum.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pygame as pg
 from scipy.integrate import odeint
+import math
+
+from sys import exit
 
 class Pendulum():
 
@@ -23,12 +26,47 @@ class Pendulum():
 
 
 	def simulate(self, time):
-		y_start = [np.pi/2, 0.01]
+		y_start = [np.pi/2, 0.0]
 		b = 10.0
 		c = 9.81
 		solutions = odeint(self.physics, y_start, time, args=(b, c))
 
 		return solutions
 
+	def draw_P(self):
+		pg.init()
+		
+		# window setup
+		self.screen = pg.display.set_mode((800, 600))
+		pg.display.set_caption('Chaos Pendulum')
+		icon = pg.image.load('img\pendulum.png')
+		pg.display.set_icon(icon)
+		clock = pg.time.Clock()
+
+		pend_end_x = 100
+		pend_end_y = 100
+
+		angle = 0
+
+		running = True
+		while running:
+			for event in pg.event.get():
+				if event.type == pg.QUIT:
+					pg.quit()
+
+			self.screen.fill((1, 1, 1))
+
+			pg.draw.circle(self.screen, (255, 255, 255), (400, 150), 25)
+			pg.draw.line(self.screen, (255, 255, 255), (400, 150), (400, 450), 6)
+			pg.draw.line(self.screen, (255, 255, 255), (360, 450), (440, 450), 8)
 
 
+
+			pg.draw.line(self.screen, (0, 100, 0), (400, 150), (pend_end_x*math.cos(angle) + 400, pend_end_y*math.sin(angle) + 150), 6)
+
+			angle += 0.01
+
+			pg.display.update()
+			clock.tick(120)
+
+		
